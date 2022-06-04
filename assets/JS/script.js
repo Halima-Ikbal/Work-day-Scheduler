762a8bae251061bdcfc3efda7978a02819eb0f3a
Halima-Ikbal <100046854+Halima-Ikbal@users.noreply.github.com>
2022-06-04 01:02:10 -0500

    edited HTML and style sheet time block

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,15 +1,20 @@
-// Display current day and date
 
-var currentDayEl = $('#currentDay'); 
-var currentDay = moment().format('dddd, MMMM Do');
-currentDayEl.text(currentDay); 
+// Display today's day and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-//create function to save task
-function saveTask(hour, task) {
-    localStorage.setItem(hour, task);
-}
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
 
+   
 
 
 
